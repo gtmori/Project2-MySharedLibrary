f2f41f50d0f11c50f7c63e26344d7195cbe1c3d9
Guilherme Tadanori Mori <gtm.mori@gmail.com>
2019-10-08 17:21:56 -0300

    adjust for heroku

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 // =====================================================================================================================================
 // Connection with database
 mongoose
-  .connect('mongodb://localhost/SharedLibrary', {useNewUrlParser: true, useUnifiedTopology: true})
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
